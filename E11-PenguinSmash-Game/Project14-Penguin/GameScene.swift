bdcf31295feb38852ad7f37a90201e4496be6da1
Mhmdckrk <mahmudckrk@gmail.com>
2023-12-25 18:53:20 +0300

    First Launch

---
+++ E11-PenguinSmash-Game/Project14-Penguin/GameScene.swift
@@ -0,0 +1,159 @@
+//
+//  GameScene.swift
+//  Project14-Penguin
+//
+//  Created by Mahmud CIKRIK on 21.10.2023.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    var slots = [WhackSlot]()
+    var popupTime = 0.85
+    var numRounds = 0
+
+    var finalScore: SKLabelNode!
+    var gameScore: SKLabelNode!
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+    
+    override func didMove(to view: SKView) {
+        
+        let background = SKSpriteNode(imageNamed: "whackBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+                
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.position = CGPoint(x: 8, y: 8)
+        gameScore.text = "Score: 0"
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
+        
+
+        
+        for i in 0..<5 { createSlot(at: CGPoint(x: 100 + ( i * 170), y: 410))}
+        for i in 0..<4 { createSlot(at: CGPoint(x: 180 + ( i * 170), y: 320))}
+        for i in 0..<5 { createSlot(at: CGPoint(x: 100 + ( i * 170), y: 230))}
+        for i in 0..<4 { createSlot(at: CGPoint(x: 180 + ( i * 170), y: 140))}
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            [weak self] in self?.createEnemy()
+        }
+        // bundan sonra tekrar tekrar kendisini çağıracak bu sayede
+
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        
+        
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let tappedNodes = nodes(at: location)
+        
+        for node in tappedNodes {
+            
+            guard let whackSlot = node.parent?.parent as? WhackSlot else { continue }
+            
+            if !whackSlot.isVisible { continue }
+            if whackSlot.isHit { continue }
+            whackSlot.hit()
+            
+            if let smokeEffect = SKEmitterNode(fileNamed: "smokeEffect") {
+                smokeEffect.position = whackSlot.position
+                smokeEffect.zPosition = 1
+                addChild(smokeEffect)
+            }
+            
+            if node.name == "charFriend" {
+                
+                score -= 5
+            
+                run(SKAction.playSoundFileNamed("whackBad.caf", waitForCompletion: false))
+                
+            } else if node.name == "charEnemy" {
+                
+        
+                
+                whackSlot.charNode.xScale = 0.85
+                whackSlot.charNode.yScale = 0.85
+                
+              
+                score += 1
+                
+                run(SKAction.playSoundFileNamed("whack.caf", waitForCompletion: false))
+
+                
+                
+            }
+            
+        }
+        
+    }
+    
+    func createSlot (at position: CGPoint) {
+        let slot = WhackSlot()
+        slot.configure(at: position)
+        addChild(slot)
+        slots.append(slot)
+        
+    }
+    
+    func createEnemy() {
+        
+        numRounds += 1
+        popupTime *= 0.991
+        
+        if numRounds >= 30 {
+            for slot in slots {
+                slot.hide()
+            }
+            
+            let finalScore = SKLabelNode(fontNamed: "Helvetica")
+            finalScore.text = "Your Score is \(score)"
+            finalScore.fontSize = 48
+            finalScore.horizontalAlignmentMode = .center
+            finalScore.position = CGPoint(x: 512, y: 284)
+            finalScore.zPosition = 2
+            addChild(finalScore)
+
+            
+            let gameOver = SKSpriteNode(imageNamed: "gameOver")
+            gameOver.position = CGPoint(x: 512, y: 384)
+            gameOver.zPosition = 1
+            addChild(gameOver)
+            run(SKAction.playSoundFileNamed("gameOver.m4a", waitForCompletion: false))
+            return
+        }
+        
+        slots.shuffle()
+        slots[0].show(hideTime: popupTime)
+        
+        if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popupTime)}
+        if Int.random(in: 0...12) > 8 { slots[2].show(hideTime: popupTime)}
+        if Int.random(in: 0...12) > 10 { slots[3].show(hideTime: popupTime)}
+        if Int.random(in: 0...12) > 11 { slots[4].show(hideTime: popupTime)}
+        
+        let minDelay = popupTime / 2.0
+        let maxDelay = popupTime * 2
+        let delay = Double.random(in: minDelay...maxDelay)
+        print(slots[0])
+        
+        // bu kod bu fonksiyonun kendi kendine çalışmasını sağlıyormuş?
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+            [weak self] in self?.createEnemy()
+            
+        }
+
+        
+    }
+    
+}
+
